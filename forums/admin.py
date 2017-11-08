b52cb8edb1baa235612fd20da7b736d49155c0d8
Andrew <andr_papusha@ukr.net>
2017-11-08 16:17:49 +0200

    End of the second part

--- forums/admin.py
+++ forums/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Board)
+admin.site.register(models.Topic)
+admin.site.register(models.Post)
--- forums/views.py
+++ forums/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from . import models
+
 def home(request):
-    return HttpResponse('Hello world!')
+    boards = models.Board.objects.all()
+    return render(request, 'forums/boards.html', {'boards': boards})
+    # return HttpResponse('Hello world!')
